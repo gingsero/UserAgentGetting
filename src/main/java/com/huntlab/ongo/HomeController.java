083f5de599100dcaa64dfdd7e28b054e5b35b463
DaeHui.KIM <DaeHui.KIM@systrangroup.com>
2017-11-19 01:20:13 +0900

    Add /index request mapping

--- src/main/java/com/huntlab/ongo/HomeController.java
+++ src/main/java/com/huntlab/ongo/HomeController.java
@@ -4,12 +4,16 @@ import java.text.DateFormat;
 import java.util.Date;
 import java.util.Locale;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
 
 /**
  * Handles requests for the application home page.
@@ -34,6 +38,20 @@ public class HomeController {
 		model.addAttribute("serverTime", formattedDate );
 		
 		return "home";
+	}
+	
+	@RequestMapping("/index")
+	public ModelAndView requsetUserAgent(HttpServletRequest req, HttpServletResponse resp) {
+		logger.info("requestUserAgent()");
+		ModelAndView mav = new ModelAndView();
+		mav.setViewName("index");
+		
+		
+		
+		
+		
+		
+		return mav;
 	}
 	
 }
