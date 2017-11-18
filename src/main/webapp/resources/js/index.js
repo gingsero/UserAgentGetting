2b9071cfe7b87f408daa779486bf02b2bc8945fe
DaeHui.KIM <DaeHui.KIM@systrangroup.com>
2017-11-19 01:21:23 +0900

    Change initializing logic in index.js

--- src/main/webapp/resources/js/index.js
+++ src/main/webapp/resources/js/index.js
@@ -2,3 +2,27 @@
  * 
  */
 
+var button_getter;
+
+
+window.onload = function(){
+	console.log("window.onload()");
+	button_getter = document.getElementById("getbutton");
+	
+	
+}
+
+
+function executeGetting(){
+	console.log("executeGetting()");
+	$.ajax({
+		type:"GET",
+		url:"/",
+		success: function(){
+			
+		},
+		error: function(){
+			
+		}
+	});
+}
